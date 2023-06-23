c959e8a9e022312a8e825fff4de3b69ab1ae1fe5
Qweexexe <105299343+Qweexexe@users.noreply.github.com>
2023-06-23 02:08:38 +0000

    deleted useless files

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,7 @@
 import {combineReducers, createStore} from "redux";
 import {reducer} from "../reducers/reducer";
 import {orderReducer} from "../reducers/ordersReducer";
-import Swal from "sweetalert2";
+
 
 
 const rootReducer = combineReducers({
